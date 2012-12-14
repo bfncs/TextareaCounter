4837fa5e260a6209c712a31160292e1fcdf7c76f
Marc Löhe <marc@marcloehe.de>
2012-12-14 14:59:53 +0100

    Fixed leftover log statement and indentation

--- TextareaCounter.js
+++ TextareaCounter.js
@@ -1,44 +1,46 @@
 (function ($) {
 
-    $.fn.TextareaCounter = function(options) {
-      var opts = $.extend($.fn.TextareaCounter.defaults, options);
-      return this.each(function() {
-        var $this = $(this),
-          $status = $this.parent().find(opts.statusSelector),
-          $statusCount = $status.find(opts.statusCountSelector),
-          maxLen = $this.attr(opts.maxLengthAttribute),
-          countFn = opts.countFn,
-          lastVal = $this.val();
-        $this.bind('keyup change', function() {
-          var left = maxLen - countFn($this.val());
-          console.log(left);
-          if (0 > left) {
-            if (!$this.hasClass('ui-state-error')) {
-              $this.val(lastVal);
-              $status.addClass('ui-state-error');
-              $statusCount.text('0');
-            }
-          } else {
-            lastVal = $this.val();
-            $statusCount.text(left);
-            if (0 < left) $status.removeClass('ui-state-error');
+  'use strict';
+  $.fn.TextareaCounter = function (options) {
+    var opts = $.extend($.fn.TextareaCounter.defaults, options);
+    return this.each(function() {
+      var $this = $(this),
+        $status = $this.parent().find(opts.statusSelector),
+        $statusCount = $status.find(opts.statusCountSelector),
+        maxLen = $this.attr(opts.maxLengthAttribute),
+        countFn = opts.countFn,
+        lastVal = $this.val();
+      $this.bind('keyup change', function () {
+        var left = maxLen - countFn($this.val());
+        if (0 > left) {
+          if (!$this.hasClass('ui-state-error')) {
+            $this.val(lastVal);
+            $status.addClass('ui-state-error');
+            $statusCount.text('0');
           }
-        });
+        } else {
+          lastVal = $this.val();
+          $statusCount.text(left);
+          if (0 < left) {
+            $status.removeClass('ui-state-error');
+          }
+        }
       });
-    };
-    $.fn.TextareaCounter.defaults = {
-      'statusSelector': 'span.counterChars',
-      'statusCountSelector': 'span',
-      'maxLengthAttribute': 'data-maxchars',
-      'countFn': function(val) {return val.trim().replace(/\r?\n/g, '\r\n').length;}
-    };
+    });
+  };
+  $.fn.TextareaCounter.defaults = {
+    'statusSelector': 'span.counterChars',
+    'statusCountSelector': 'span',
+    'maxLengthAttribute': 'data-maxchars',
+    'countFn': function (val) {return val.trim().replace(/\r?\n/g, '\r\n').length;}
+  };
 
   $(document).ready(function () {
     $('textarea[data-maxchars]').TextareaCounter();
     $('textarea[data-maxwords]').TextareaCounter({
       'statusSelector': 'span.counterWords',
       'maxLengthAttribute': 'data-maxwords',
-      'countFn': function(val) {return val.trim().split(/\s+/).length;}
+      'countFn': function (val) {return val.trim().split(/\s+/).length;}
     });
   });
 
